71672fc46ce29508bd44bea058749b15835cbd77
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-19 14:45:14 -0300

    Added simple setup for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest"/>
+/// <reference types="vite/client"/>
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react({
+      include: "**/*.tsx",
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: `jsdom`,
+    setupFiles: "./src/__test__/setup.tsx",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
+});
