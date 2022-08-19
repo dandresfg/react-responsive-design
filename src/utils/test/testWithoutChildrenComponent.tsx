353b063b90d0d87662eb13c17470088eccf9936f
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-19 16:20:42 -0300

    Delete testWithoutChildrenComponent.tsx

--- src/utils/test/testWithoutChildrenComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ReactChildren } from "../../common/types";
-
-const TestWithoutChildrenComponent = () => {
-  return <div>This is a test component without children</div>;
-};
-
-export default TestWithoutChildrenComponent;
