a5ba9f3deb318507ce370c3b6bebd677acecfc06
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-19 18:31:15 -0300

    fixed matchMediaDefinition

--- src/utils/test/matchMediaDefinition.ts
+++ src/utils/test/matchMediaDefinition.ts
@@ -3,16 +3,19 @@ import { vi } from "vitest";
 const matchMediaDefinition = () => {
   Object.defineProperty(window, "matchMedia", {
     writable: true,
-    value: vi.fn().mockImplementation((query) => ({
-      matches: true,
-      media: query,
-      onchange: null,
-      addListener: vi.fn(), // Deprecated
-      removeListener: vi.fn(), // Deprecated
-      addEventListener: vi.fn(),
-      removeEventListener: vi.fn(),
-      dispatchEvent: vi.fn(),
-    })),
+    value: vi.fn().mockImplementation((query) => {
+      const values = query.match(/\d*(?=px)/g);
+      return {
+        matches:
+          Number(values[0]) < window.innerWidth &&
+          Number(values[2]) > window.innerWidth,
+        media: query,
+        onchange: null,
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+      };
+    }),
   });
 };
 
