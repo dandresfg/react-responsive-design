9772f282cd392d133c024ee8ca0ecf1da5fb8538
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-19 16:41:16 -0300

    Added test for all media query

--- src/__test__/hidden.test.tsx
+++ src/__test__/hidden.test.tsx
@@ -4,6 +4,13 @@ import Hidden from "../lib/hidden";
 import matchMediaDefinition from "../utils/test/matchMediaDefinition";
 import resizeToDefinition from "../utils/test/resizeToDefinition";
 
+// Breakpoints:
+//              xs: 0,
+//              sm: 600,
+//              md: 992,
+//              lg: 1200,
+//              xl: 1536
+
 describe("Hidden Component", () => {
   afterEach(cleanup);
 
@@ -35,11 +42,11 @@ describe("Hidden Component", () => {
         "Hidden shouldn't render"
       ).length(0);
     }),
-    it("Shouldn't render on sm media query", () => {
+    it("Shouldn't render on 'sm' media query", () => {
       const hiddenComponent = render(
         <div role="parent">
           <Hidden media="sm">
-            <p>Hello From Hidden</p>
+            <p>Hello From Hidden out of Small Query</p>
           </Hidden>
         </div>
       );
@@ -47,8 +54,8 @@ describe("Hidden Component", () => {
       const parentComponent = hiddenComponent.getByRole("parent");
 
       expect(
-        hiddenComponent.getByText("Hello From Hidden"),
-        "Hidden should render"
+        hiddenComponent.getByText("Hello From Hidden out of Small Query"),
+        "Hidden should render on all breakpoints except Small"
       ).toBeTruthy();
 
       //Resize event
@@ -58,7 +65,85 @@ describe("Hidden Component", () => {
 
       expect(
         parentComponent.getElementsByTagName("p"),
-        "Hidden shouldn't render"
+        "Hidden shouldn't render on Small Breakpoint"
+      ).length(0);
+    }),
+    it("Shouldn't render on 'md' media query", () => {
+      const hiddenComponent = render(
+        <div role="parent">
+          <Hidden media="md">
+            <p>Hello From Hidden out of Medium Query</p>
+          </Hidden>
+        </div>
+      );
+
+      const parentComponent = hiddenComponent.getByRole("parent");
+
+      expect(
+        hiddenComponent.getByText("Hello From Hidden out of Medium Query"),
+        "Hidden should render on all breakpoints except Medium"
+      ).toBeTruthy();
+
+      //Resize event
+      window.resizeTo = resizeToDefinition;
+      window.resizeTo(800, 500);
+      fireEvent(window, new Event("resize"));
+
+      expect(
+        parentComponent.getElementsByTagName("p"),
+        "Hidden shouldn't render on Medium Breakpoint"
+      ).length(0);
+    }),
+    it("Shouldn't render on 'lg' media query", () => {
+      const hiddenComponent = render(
+        <div role="parent">
+          <Hidden media="lg">
+            <p>Hello From Hidden out of Large Query</p>
+          </Hidden>
+        </div>
+      );
+
+      const parentComponent = hiddenComponent.getByRole("parent");
+
+      expect(
+        hiddenComponent.getByText("Hello From Hidden out of Large Query"),
+        "Hidden should render on all breakpoints except Large"
+      ).toBeTruthy();
+
+      //Resize event
+      window.resizeTo = resizeToDefinition;
+      window.resizeTo(1000, 500);
+      fireEvent(window, new Event("resize"));
+
+      expect(
+        parentComponent.getElementsByTagName("p"),
+        "Hidden shouldn't render on Large Breakpoint"
+      ).length(0);
+    }),
+    it("Shouldn't render on 'xl' media query", () => {
+      const hiddenComponent = render(
+        <div role="parent">
+          <Hidden media="xl">
+            <p>Hello From Hidden out of Extra Large Query</p>
+          </Hidden>
+        </div>
+      );
+
+      const parentComponent = hiddenComponent.getByRole("parent");
+
+      expect(
+        hiddenComponent.getByText("Hello From Hidden out of Extra Large Query"),
+        "Hidden should render on all breakpoints except Extra Large"
+      ).toBeTruthy();
+
+      //Resize event
+      window.resizeTo = resizeToDefinition;
+      window.resizeTo(1400, 500);
+      fireEvent(window, new Event("resize"));
+
+      expect(
+        parentComponent.getElementsByTagName("p"),
+        "Hidden shouldn't render on Extra Large Breakpoints"
       ).length(0);
     });
 });
