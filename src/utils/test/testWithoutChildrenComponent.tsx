7dd3ab5d1a67a613e54811dbba8de67bba6bc507
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-19 14:51:40 -0300

    Create testWithoutChildrenComponent.tsx

---
+++ src/utils/test/testWithoutChildrenComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactChildren } from "../../common/types";
+
+const TestWithoutChildrenComponent = () => {
+  return <div>This is a test component without children</div>;
+};
+
+export default TestWithoutChildrenComponent;
