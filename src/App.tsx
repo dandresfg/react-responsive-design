1ae7b053144bf1123a44a2e92245341e5df85436
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-25 12:54:42 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { Hidden, MediaProvider } from "./lib"
+import { Hidden, MediaProvider } from "./lib";
 
 function App() {
   return (
     <section>
       <MediaProvider>
-
-        {/* Both can be read in sx and xl */}
+        {/* Both can be read in xs and xl */}
         <p>
-          <Hidden media={["sm", "lg"]}>
-            {' Hola soy '}
-          </Hidden>
-          <Hidden media={"md"}>
-            {' Skywalker '}
-          </Hidden>
+          <Hidden media={["xs"]}>{" im out of xs"}</Hidden>
+          <Hidden media={["sm", "lg"]}>{" im out of sm or lg"}</Hidden>
+          <Hidden media={"md"}>{" im out of md "}</Hidden>
         </p>
-        
       </MediaProvider>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
