ed4317fd284d451df03f47f73924a8fd24af9e85
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-27 20:45:58 -0300

    now defaultBreakpoints are from materialUi

--- src/theme/breakpoints.tsx
+++ src/theme/breakpoints.tsx
@@ -1,20 +1,15 @@
+import breakpointsDefinitions from "./breakpointOptions";
+
 export type breakpoint = "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
 
 export type breakpointList = {
-    "xs": number,
-    "sm": number,
-    "md": number,
-    "lg": number,
-    "xl": number,
-    "2xl"?: number
+  xs: number;
+  sm: number;
+  md: number;
+  lg: number;
+  xl: number;
+  "2xl"?: number;
 };
 
-export type breakpointOptions = "bootstrap" | "tailwind" | "material"
-
-export const defaultBreakpoints: breakpointList = {
-    "xs": 0,
-    "sm": 600,
-    "md": 900,
-    "lg": 1200,
-    "xl": 1536
-}
+export const defaultBreakpoints: breakpointList =
+  breakpointsDefinitions["material"];
