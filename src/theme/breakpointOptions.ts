b593d3807e297c0e4c3d8c7e15cf162092d43d9b
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-27 20:45:28 -0300

    added breakpoints options to preload breakpoints

---
+++ src/theme/breakpointOptions.ts
@@ -0,0 +1,59 @@
+import { breakpointList } from "./breakpoints";
+
+export type breakpointOptions =
+  // | "ant-design" | "chakra"
+  "bootstrap" | "bulma" | "material" | "tailwind";
+
+type BreakpointsDefinitions = {
+  [key: string]: breakpointList;
+};
+
+const breakpointsDefinitions: BreakpointsDefinitions = {
+  // "ant-desing": {  This might break anything else
+  //   xs: '480px',
+  //   sm: '576px',
+  //   md: '768px',
+  //   lg: '992px',
+  //   xl: '1200px',
+  //   2xl: '1600px',
+  // },
+  bootstrap: {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+    "2xl": 1400,
+  },
+  bulma: {
+    xs: 0,
+    sm: 768,
+    md: 1024,
+    lg: 1216,
+    xl: 1408,
+  },
+  // chakra: {
+  //   sm: '30em',
+  //   md: '48em',
+  //   lg: '62em',
+  //   xl: '80em',
+  //   '2xl': '96em',
+  // },
+  material: {
+    xs: 0,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
+  },
+  tailwind: {
+    xs: 0,
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+    "2xl": 1536,
+  },
+};
+
+export default breakpointsDefinitions;
