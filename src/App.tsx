8aa47652ea131c708b26c50013831b8fbae6ae6d
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-29 13:00:48 -0300

    just testing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
     <section>
       <Hidden media="xs">{" im out of context check console"}</Hidden>
       {/* Both can be read in xs and xl */}
-      <MediaProvider>
+      <MediaProvider
+        breakpoints={{ xs: 0, sm: 200, md: 500, lg: 1000, xl: 1200 }}
+      >
         <p>
           <Hidden media={["sm", "lg"]}>{" im out of sm or lg"}</Hidden>
           <Hidden media={"md"}>{" im out of md "}</Hidden>
